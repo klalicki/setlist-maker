229cf684aa8f9b03cce952f77ed784e213e5bef8
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-06-06 22:11:36 -0400

    fixes spelling error in username lol

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  uesrname: String,
+  username: String,
   password: String,
   email: String,
   albums: [String],
@@ -10,20 +10,25 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 // UTILITY
 const getUser = async (username) => {
-  return await User.findOne({ username: username });
+  return User.findOne({ username: username }).exec();
 };
 
 // CREATE
 const register = async (username, password, email) => {
-  const user = getUser(username);
+  const user = await getUser(username);
+  console.log(user);
   if (user) throw Error("Username already in use!");
 
-  const newUser = await User.create({ username: username, password: password });
+  const newUser = await User.create({
+    username: username,
+    password: password,
+    email: email,
+  });
   return newUser;
 };
 // READ
 const login = async (username, password) => {
-  const user = getUser(username);
+  const user = await getUser(username);
   if (!user) throw Error("User not found!");
   if (user.password !== password) throw Error("Incorrect password!");
   return user;
