1f762e868356f629f8d37500b88c3f36beae2bcf
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-06-06 21:50:30 -0400

    finishes creating routes for user

--- server/routes/user.js
+++ server/routes/user.js
@@ -15,7 +15,33 @@ router.post("/login", async (req, res) => {
 
 router.post("/register", async (req, res) => {
   try {
+    const newUser = await User.register(
+      req.body.username,
+      req.body.password,
+      req.body.email
+    );
+    res.send({ ...newUser, password: undefined });
   } catch (error) {
     res.status(401).send({ message: error.message });
   }
 });
+
+router.put("/update", async (req, res) => {
+  try {
+    const user = await User.updatePassword(req.body.id, req.body.password);
+    res.send({ ...user, password: undefined });
+  } catch (error) {
+    res.status(401).send({ message: error.message });
+  }
+});
+
+router.delete("/delete", async (req, res) => {
+  try {
+    const user = await User.deleteUser(req.body.id);
+    // res.send({ ...user, password: undefined });
+  } catch (error) {
+    res.status(401).send({ message: error.message });
+  }
+});
+
+module.exports = router;
