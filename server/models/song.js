badc8a017975649256f1bdd61f980b98d9177fc0
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-06-09 23:17:46 -0400

    creates model for songs

---
+++ server/models/song.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const songSchema = new mongoose.Schema({
+  title: String,
+  length: Number,
+  key: String,
+  mood: String,
+  performanceNotes: String,
+});
+
+const Song = mongoose.model("Song", songSchema);
+
+const createSong = async (title, length, key, mood, performanceNotes) => {
+  const newSong = await Song.create({
+    title: title,
+    length: length,
+    key: key,
+    mood: mood,
+    performanceNotes: performanceNotes,
+  });
+  return newSong.toObject();
+};
+
+const createMultipleSongs = async (songInfo) => {
+  const newSongs = await songInfo.map(
+    ({ title, length, key, mood, performanceNotes }) => {
+      return createSong(title, length, key, mood, performanceNotes);
+    }
+  );
+  return newSongs;
+};
+
+const getSong = async (songID) => {
+  const song = await Song.findById(songID);
+  return song.toObject();
+};
+
+const updateSong = async (songID, newSongInfo) => {
+  await Song.updateOne({ _id: songID }, { $set: { ...newSongInfo } });
+  const updatedSong = await Song.findById(songID);
+
+  return updatedSong;
+};
+
+const deleteSong = async (songID) => {
+  await Song.deleteOne({ _id: songID });
+};
+
+module.exports = {
+  createSong,
+  createMultipleSongs,
+  getSong,
+  updateSong,
+  deleteSong,
+};
