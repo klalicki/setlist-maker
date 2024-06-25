abe22b39168836c2bd4a60b05d7199982f41b94c
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-06-24 22:14:42 -0400

    sets up basic NavBar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,20 @@
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <nav class="navbar navbar-expand-sm ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          Setlist Maker
+        </a>
+
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link active" href="/login">
+              Login
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default NavBar;
