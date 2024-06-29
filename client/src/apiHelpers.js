11cc07b22a6697567d2646689060cb53f9c67180
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-06-29 14:47:32 -0400

    adds helper function for data fetching

---
+++ client/src/apiHelpers.js
@@ -0,0 +1,13 @@
+export const fetchData = async (route = "", data = {}, methodType = "GET") => {
+  const response = await fetch(`http://localhost:5000/${route}`, {
+    method: methodType,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  if (response.ok) {
+    return await response.json();
+    
+  } else {
+    throw await response.json()
+  }
+};
