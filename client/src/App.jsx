9bb1c0f0f95c0b10d06ea9909afbaeb0749dd3d5
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-03 12:39:23 -0400

    adds context wrapper to project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,21 @@
 import NavBar from "./components/NavBar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./context/userContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </BrowserRouter>
-      {/* <div className="container">
-        <div className="row gx-3 gy-3 pt-3">
-          <div className="col col-12 col-md-4">
-            <LoginForm />
-          </div>
-          <div className="col col-12 col-md-4">
-            <RegisterForm />
-          </div>
-        </div>
-      </div> */}
-    </div>
+    <UserContextProvider>
+      <div className="app">
+        <NavBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </UserContextProvider>
   );
 }
 
