402bf4a8a27c0fbb466698fd2814210feaf71216
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-04 15:26:02 -0400

    adds authenticated flag to nav temporarily

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
+import { UserContext } from "../context/UserContext";
+import { useContext, useState } from "react";
+
 const NavBar = () => {
+  const { isAuthenticated } = useContext(UserContext);
+
   return (
     <nav class="navbar navbar-expand-sm border-bottom border-primary border-2 shadow-sm">
       <div class="container-fluid">
         <a class="navbar-brand" href="/">
           Setlist Maker
         </a>
-
+        {isAuthenticated ? "Authenticated!!" : "Not Authenticated"}
         <ul class="navbar-nav d-flex flex-row">
           <li class="nav-item px-2">
             <a class="nav-link " href="/login">
