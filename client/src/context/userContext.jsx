b553a5d1b186ec2f4deb50356fa6f88d26c66668
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-03 23:12:29 -0400

    adds notes to context provider

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 
 const UserContext = createContext(null);
+// todo - set up login/logout functions, user object, isLoggedIn on this context provider
 
 const UserContextProvider = ({ children }) => {
   return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
