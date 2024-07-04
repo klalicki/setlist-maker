a198a780bc475254f7ed40c49d150f9fa76765c1
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-04 17:02:07 -0400

    adds route for profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
 import { UserContextProvider } from "./context/UserContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -11,7 +12,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/register" element={<RegisterPage />} />{" "}
+            <Route path="/profile" element={<ProfilePage />} />
           </Routes>
         </BrowserRouter>
       </div>
