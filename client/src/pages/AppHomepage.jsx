ded6f6b4ca270db7eeb42c0f7c3005719e5162fc
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-05 17:28:16 -0400

    adds welcome + username to logged in page

--- client/src/pages/AppHomepage.jsx
+++ client/src/pages/AppHomepage.jsx
@@ -1,7 +1,18 @@
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
 const AppHomepage = () => {
+  const { userInfo } = useContext(UserContext);
+  console.log(userInfo);
   return (
-    <div>
-      <h1>Welcome! logged in </h1>
+    <div className="container">
+      <div className="row">
+        <h2>Welcome, {userInfo.username}!</h2>
+      </div>
+      <div className="row">
+        <div className="col col-md-6">
+          <h2>Setlists</h2>
+        </div>
+      </div>
     </div>
   );
 };
