83fa64ed5a5183b9ba5e84039aac7eb6f8d4c2e1
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-05 17:25:14 -0400

    adds bullet points to landing page

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -9,8 +9,32 @@ const Homepage = () => {
             <h1>Welcome to Setlist Maker!</h1>
             <p>
               Setlist Maker is an app to help artists manage their setlists for
-              live performances.
+              live performances. The app will eventually have the following
+              features:
             </p>
+            <ul>
+              <li>
+                Import artist discography and track information from Spotify
+                and/or Bandcamp
+              </li>
+              <li>
+                Add performance notes to each song, such as instrument or tuning
+                info
+              </li>
+              <li>
+                Build setlists from discography, keeping track of total setlist
+                length
+              </li>
+              <li>
+                Print setlists or share them as a URL to a mobile-friendly
+                layout
+              </li>
+              <li>
+                Keep track of performances of each setlist, and include notes
+                about how the performance went
+              </li>
+              <li>...and much more!</li>
+            </ul>
           </div>
           <div className="col-12 col-md-6 col-lg-4 p-3 gap-2 border border-primary border-2 d-flex flex-column">
             <LoginForm />
