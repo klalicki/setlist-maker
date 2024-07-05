0073feeb00faea17ec1872341b2141436f462df3
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-05 17:08:12 -0400

    improves layout of homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,15 +4,15 @@ const Homepage = () => {
   return (
     <>
       <div className="container">
-        <div className="row">
-          <div className="col col-md-8">
+        <div className="row py-3">
+          <div className="col-12 col-md-6 col-lg-8 d-flex flex-column justify-content-center">
             <h1>Welcome to Setlist Maker!</h1>
             <p>
               Setlist Maker is an app to help artists manage their setlists for
               live performances.
             </p>
           </div>
-          <div className="col col-md-4 p-3 gap-2 border border-primary border-2 d-flex flex-column">
+          <div className="col-12 col-md-6 col-lg-4 p-3 gap-2 border border-primary border-2 d-flex flex-column">
             <LoginForm />
             <p className="mb-0">
               Not a user yet? Sign up <Link to="/register">here.</Link>
