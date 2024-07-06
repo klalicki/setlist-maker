b9786b7f23185fa0adc0b85bde1e11066c2ea470
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-05 22:08:26 -0400

    spruces up profile page a bit

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,10 +1,14 @@
 import ChangePasswordForm from "../forms/ChangePasswordForm";
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
 const ProfilePage = () => {
+  const { userInfo } = useContext(UserContext);
   return (
     <div className="container">
       <div className="row justify-content-center py-3">
         <div className="col col-12 col-sm-6 col-md-4">
           <h1>Profile</h1>
+          <p className="lead">Hi there, {userInfo.username}! </p>
           <p>
             Eventually there will be other things here, but for now you can
             update your password on this page.
