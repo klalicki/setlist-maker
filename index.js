bf79c215b94758f04fbb9651c78dcea6b325b57b
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-06 18:09:34 -0400

    forces port to 5000

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/api/performance", performanceRoutes);
 
 app.use(express.static(__dirname + "/client/build"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
