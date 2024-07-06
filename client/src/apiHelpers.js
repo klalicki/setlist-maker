8de321838a1dbf83351a899ec70246a14203e39d
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-06 18:20:33 -0400

    tries removing 'localhost' from api calls

--- client/src/apiHelpers.js
+++ client/src/apiHelpers.js
@@ -1,5 +1,5 @@
 export const fetchData = async (route = "", data = {}, methodType = "get") => {
-  const response = await fetch(`http://localhost:5000/api/${route}`, {
+  const response = await fetch(`/api/${route}`, {
     method: methodType,
     headers: { "Content-Type": "application/json" },
     body: methodType === "get" ? null : JSON.stringify(data),
