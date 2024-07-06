8d928fc29ec220cfaa518b12496e366562167730
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-07-06 17:18:23 -0400

    continuing to set up build for hosting

--- index.js
+++ index.js
@@ -35,10 +35,7 @@ app.use("/api/song", songRoutes);
 app.use("/api/setlist", setlistRoutes);
 app.use("/api/performance", performanceRoutes);
 
-app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public", "index.html"));
-});
+app.use(express.static(__dirname + "/client/build"));
 
 const PORT = process.env.PORT || 5000;
 
